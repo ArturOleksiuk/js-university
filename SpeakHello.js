2c7900eff3a5ef5bb0a7e476ef373fa064ff016b
ArturOleksiuk <114471702+ArturOleksiuk@users.noreply.github.com>
2025-03-20 02:38:12 +0200

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-    let speakWord = "Hello";
-    let helloGreeter = {};
-    helloGreeter.speak = function (name) {
-        console.log(speakWord + " " + name);
-    };
-    window.helloGreeter = helloGreeter;
-})(window);
