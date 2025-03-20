ab1821e8ac91d8677b0b3bb43bb87505ded8de2b
ArturOleksiuk <114471702+ArturOleksiuk@users.noreply.github.com>
2025-03-20 02:41:02 +0200

    Add files via upload

---
+++ lab_3/script.js
@@ -0,0 +1,19 @@
+(function(){
+    let names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+    for (let name of names) {
+        if (name.charAt(0).toLowerCase() === "j") {
+          byeSpeaker.speak(name);
+        } else {
+          helloGreeter.speak(name);
+        }
+    }
+    console.log("\nAdditional selection based on name length:");
+    const LENGTH_TRESHOLD = 5;
+    for(let name of names){
+        if (names[i].length > LENGTH_THRESHOLD) {
+            console.log(`Long name detected: Hello ${names[i]}`);
+        } else {
+            console.log(`Short name detected: Goodbye ${names[i]}`);
+        }
+    }
+})();
